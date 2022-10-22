da94c55b05ca19a95a08f10c1d261b5297bda113
Sapper <sapper0922@gmail.com>
2022-10-22 14:12:46 -0600

    first commit

---
+++ src/Game.java
@@ -0,0 +1,23 @@
+
+import javax.swing.JFrame;
+
+public class Game {
+    public static void main(String[] args) throws Exception {
+        
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(true);
+        window.setTitle("2d Adventure.");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        gamePanel.startGameThread();
+
+    }
+}
