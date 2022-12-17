72ae624b0bf704001b3c9f9ba42ddbc1880398ec
Sapper <sapper0922@gmail.com>
2022-12-17 12:15:17 -0700

    Added World Map and Camera.

--- src/Main/GamePanel.java
+++ src/Main/GamePanel.java
@@ -21,12 +21,18 @@ public class GamePanel extends JPanel implements Runnable{
     public final int screenWidth = tileSize * maxScreenCol; // 768 pixels
     public final int screenHeight = tileSize * maxScreenRow; // 576 pixels
 
+    // World Settings
+    public final int maxWorldCol = 50;
+    public final int maxWorldRow = 50;
+    public final int worldwidth = tileSize * maxWorldCol;
+    public final int worldHeight = tileSize * maxWorldRow;
+
     int FPS = 60;
 
     TileManager tileM = new TileManager(this);
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
-    Player player = new Player(this,keyH);
+    public Player player = new Player(this,keyH);
 
     public GamePanel() {
 
