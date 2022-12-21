1344280e3421236b76e8d9983908af05aed3ca19
Sapper <sapper0922@gmail.com>
2022-12-21 16:59:08 -0700

    Added Collision to different tiles.

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -1,6 +1,7 @@
 package entity;
 
 import java.awt.image.BufferedImage;
+import java.awt.Rectangle;
 
 public class Entity {
     
@@ -12,4 +13,6 @@ public class Entity {
 
     public int spriteCounter = 0;
     public int spriteNum = 1;
+    public Rectangle solidArea;
+    public boolean collisionOn = false;
 }
